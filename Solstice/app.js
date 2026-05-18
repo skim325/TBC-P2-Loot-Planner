3743dfabbdfa161a6a5332e24ce7519f785887e7
skim325 <325skim@gmail.com>
2026-05-18 16:30:38 +0900

    added a Solstice endpoint

--- Solstice/app.js
+++ Solstice/app.js
@@ -1,5 +1,5 @@
 
-const COMMENTARY_SOURCE_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTpjRX4vWQj-oIL-ujE7mK3DVPhTeHqkZFQ9uXx5YVP6MMzxr9CSxLVrg6y2dNjrCuj2ZLW7Lvyj4R2/pub?gid=602268290&single=true&output=csv";
+const COMMENTARY_SOURCE_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSYyF5DsxemexG_UqnUnQk_w371D0VMgS-96jW2kns-tf2PXy6mnSeNWQjWg3KMXYYmKsGr5dOyz5jN/pub?output=csv";
 // Replace the line above with your Google Sheets published CSV URL later.
 
 
